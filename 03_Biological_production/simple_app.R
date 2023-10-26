f30b79bc86d4408e546f84f62ac75c935c919818
Colin Millar <colin.millar@ices.dk>
2023-10-26 12:11:03 +0200

    add agemodel function to app

--- 03_Biological_production/simple_app.R
+++ 03_Biological_production/simple_app.R
@@ -1,6 +1,33 @@
 library(shiny)
 library(ggplot2)
 
+## Functions
+
+AgeModel <- function(Ninit, M, Fmort, mat, w, Amax, Tmax, alpha, beta, v)
+{
+  N <- matrix(nrow=Amax, ncol=Tmax)
+  SSB <- numeric(Tmax)
+
+  ## Year 1
+  N[1,1] <- Ninit
+  for (a in 1:(Amax - 1)) {
+    N[a + 1, 1] <- N[a, 1] * exp(-M[a] - Fmort[a])
+  }
+  SSB[1] <- sum(N[,1] * mat * w)
+
+  ## Later years
+  for(t in 1:(Tmax-1))
+  {
+    N[1,t+1] <- (alpha*SSB[t]) / (beta+SSB[t]) + v*runif(1,-0.5,0.5)
+    for (a in 1:(Amax - 1)) {
+      N[a + 1, t + 1] <- N[a, t] * exp(-M[a] - Fmort[a])
+    }
+    SSB[t+1] <- sum(N[,t+1] * mat * w)
+  }
+
+  list(alpha=alpha, beta=beta, N=N, SSB=SSB, Rec=N[1,])
+}
+
 
 ui <- fluidPage(
 
